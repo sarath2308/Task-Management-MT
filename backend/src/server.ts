9b436b49cc55d57acc3583a4f4ebf25853e3369d
Sarath vp <162009289+sarath2308@users.noreply.github.com>
2026-03-09 18:52:21 +0530

    fix: vercel rewrites

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -35,7 +35,7 @@ async function startServer() {
 
   const PORT: number = Number(process.env.PORT) || 5000;
   server.listen(PORT, () =>
-    console.log(`Server running on http://localhost:${PORT}`),
+    console.log(`Server running on Port ${PORT}`),
   );
 
   app.use(errorHandler);
--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL_API,
+  baseURL: "/api",
   withCredentials: true,
 });
 
