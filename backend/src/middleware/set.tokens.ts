38c8b6e43f4a3a2ed30e96d50ce762764b207e2a
Sarath vp <162009289+sarath2308@users.noreply.github.com>
2026-03-09 14:02:17 +0530

    fix:token fix for login

--- backend/src/middleware/set.tokens.ts
+++ backend/src/middleware/set.tokens.ts
@@ -7,16 +7,16 @@ export const setTokens = (
 ) => {
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+    secure: true,
+    sameSite:"none",
     maxAge: 15 * 60 * 1000,
   });
 
   if (refreshToken) {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 60 * 60 * 1000,
     });
   }
